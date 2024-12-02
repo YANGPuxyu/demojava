5487b833ffbed2ca305e146c2245c66a055fcd75
YANGPuxyu <yingtao0427@gmail.com>
2024-12-02 19:13:48 +0800

    Current Version

--- src/main/java/com/chat/demo/controller/FriendshipController.java
+++ src/main/java/com/chat/demo/controller/FriendshipController.java
@@ -3,9 +3,9 @@ package com.chat.demo.controller;
 import com.chat.demo.entity.DTO.FriendshipDTO;
 import com.chat.demo.entity.Friendship;
 import com.chat.demo.entity.User;
+import com.chat.demo.response.Response;
 import com.chat.demo.service.FriendshipService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -18,30 +18,30 @@ public class FriendshipController {
     private FriendshipService friendshipService;
 
     @PostMapping("/friends")
-    public ResponseEntity<Friendship> addFriend(@RequestBody FriendshipDTO friendshipDTO) {
+    public Response<Friendship> addFriend(@RequestBody FriendshipDTO friendshipDTO) {
         User user = new User();
         user.setId(friendshipDTO.getUserId());
         User friend = new User();
         friend.setId(friendshipDTO.getFriendId());
         Friendship newFriendship = friendshipService.addFriend(user, friend);
-        return ResponseEntity.status(201).body(newFriendship);
+        return Response.success(newFriendship);
     }
 
     @GetMapping("/users/{userId}/friends")
-    public ResponseEntity<List<Friendship>> getFriends(@PathVariable Long userId) {
+    public Response<List<Friendship>> getFriends(@PathVariable Long userId) {
         User user = new User();
         user.setId(userId);
         List<Friendship> friends = friendshipService.getFriends(user);
-        return ResponseEntity.ok(friends);
+        return Response.success(friends);
     }
 
     @DeleteMapping("/users/{userId}/friends/{friendId}")
-    public ResponseEntity<Void> removeFriend(@PathVariable Long userId, @PathVariable Long friendId) {
+    public Response<Void> removeFriend(@PathVariable Long userId, @PathVariable Long friendId) {
         User user = new User();
         user.setId(userId);
         User friend = new User();
         friend.setId(friendId);
         friendshipService.removeFriend(user, friend);
-        return ResponseEntity.noContent().build();
+        return Response.success(null);
     }
 }
